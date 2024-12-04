ebd742e251a5dceccccf6b26a0ed090c1ba6bea5
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2024-12-04 16:38:23 -0300

    rename timestampToDate

--- src/components/Common/CountdownTimer/index.jsx
+++ src/components/Common/CountdownTimer/index.jsx
@@ -1,7 +1,7 @@
 import { PropTypes } from "prop-types";
 import styles from "./styles.module.css";
 import { useEffect, useState } from "react";
-import { timestampToDate } from "../../../helpers/timeStampToDate";
+import { timestampToDate } from "../../../helpers/timestampToDate";
 
 /**
  * @param {{
